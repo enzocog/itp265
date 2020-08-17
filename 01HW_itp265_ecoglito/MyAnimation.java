fef9ba7891398a5e0a226e29d90bc0de5e62b788
enzocog <enzocogjk@gmail.com>
2020-08-17 14:49:29 -0700

    first commit

---
+++ 01HW_itp265_ecoglito/MyAnimation.java
@@ -0,0 +1,19 @@
+
+/**
+ * This prints out some ASCII art
+ *
+ * @author Enzo Coglitore
+ * ITP 265, Term Year (Fall 2020), Class Session (Coffee)
+ * Assignment 00
+ * Email: ecoglito@usc.edu
+ */
+public class MyAnimation
+{
+    public static void main(){
+ 
+        System.out.println("^..^      /\n" + "/_/\\_____/\n" + "   /\\   /\\\n" + "  /  \\ /  \\\n");
+      
+        System.out.println("^..^      \\\n" + "/_/\\_____/\n" + "   /\\   /\\\n" + "  /  \\ /  \\\n");
+        
+    }
+}
